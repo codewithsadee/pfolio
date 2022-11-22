9dafa05bc37e98cc78efbe4765b2147cc6d86423
Sadee <mohammadsadee24@gmail.com>
2022-11-22 19:05:11 +0600

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,149 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * NAVBAR
+ * navbar toggle for mobile
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * header active when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function (currentSlider) {
+
+  const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+  let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = function () {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+  }
+
+  /**
+   * NEXT SLIDE
+   */
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREVIOUS SLIDE
+   */
+  const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+      currentSlidePos = totalSlidableItems;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = totalSlidableItems <= 0;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.display = 'none';
+    sliderPrevBtn.style.display = 'none';
+  }
+
+  /**
+   * slide with [shift + mouse wheel]
+   */
+
+  currentSlider.addEventListener("wheel", function (event) {
+    if (event.shiftKey && event.deltaY > 0) slideNext();
+    if (event.shiftKey && event.deltaY < 0) slidePrev();
+  });
+
+  /**
+   * RESPONSIVE
+   */
+
+  window.addEventListener("resize", function () {
+    totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    moveSliderItem();
+  });
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
---
+++ style-guide.md
@@ -0,0 +1,75 @@
+# Essential Stuff
+
+## Html import links
+
+Google font
+
+``` html
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&display=swap" rel="stylesheet">
+```
+
+Ionicon
+
+``` html
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+```
+
+---
+
+## Colors
+
+``` css
+--raisin-black: hsla(231, 10%, 14%, 1);
+--roman-silver: hsla(229, 10%, 57%, 1);
+--eerie-black: hsla(228, 9%, 10%, 1);
+--black: hsla(0, 0%, 0%, 1);
+--white: hsla(0, 0%, 100%, 1);
+--white_a10: hsla(0, 0%, 100%, 0.1);
+--white_a5: hsla(0, 0%, 100%, 0.05);
+```
+
+## Typography
+
+``` css
+--ff-syne: 'Syne', sans-serif;
+
+--fs-1: 4.8rem;
+--fs-2: 4.5rem;
+--fs-3: 4rem;
+--fs-4: 2.4rem;
+--fs-5: 2rem;
+--fs-6: 1.8rem;
+--fs-7: 1.4rem;
+--fs-8: 1.2rem;
+
+--fw-800: 800;
+--fw-700: 700;
+```
+
+## Spacing
+
+``` css
+--section-padding: 100px;
+```
+
+## Border Radius
+
+``` css
+--radius-pill: 100px;
+--radius-circle: 50%;
+--blob-radius: 52% 48% 59% 41% / 53% 40% 60% 47%;
+```
+
+## Transition
+
+``` css
+--transition-1: 0.25s ease;
+--transition-2: 0.5s ease;
+--cubic-in: cubic-bezier(0.51, 0.03, 0.64, 0.28);
+--cubic-out: cubic-bezier(0.05, 0.83, 0.52, 0.97);
+--cubic-bounce: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+--cubic-ease-out: 700ms cubic-bezier(0.17, 0.67, 0, 1.01);
+```
